4bb101dba965b4ea25c8c22559c3a96f733418dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-15 13:34:54 +0000

    Deploy atjhoendz/atjhoendz.github.io to github.com/atjhoendz/atjhoendz.github.io.git:master

---
+++ _nuxt/cb606eb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{439:function(t,e,n){"use strict";n.r(e);var l={},c=n(58),component=Object(c.a)(l,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"blue white--text pa-4 mb-4"},[e("p",[this._t("info-box",[this._v("default")])],2)])}),[],!1,null,null,null);e.default=component.exports}}]);
